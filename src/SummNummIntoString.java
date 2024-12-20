fde62033d62b29d3bb10b39c74803aedbee64212
mark <ivanchenko.mark@inbox.ru>
2024-12-20 16:18:43 +0700

    Создал метод заполнения и вывода на печать двумерного массива

--- src/SummNummIntoString.java
+++ src/SummNummIntoString.java
@@ -15,9 +15,43 @@ Sample Output:
         15
 */
 
+import java.util.Scanner;
 
 public class SummNummIntoString {
     public static void main(String[] args) {
+        TwoDimensionalArray();
+    }
 
+
+    // Метод для создания двумерного массива
+    public static void TwoDimensionalArray() {
+        Scanner in = new Scanner(System.in);
+
+        // Ввод размеров массива
+        System.out.print("Введите количество строк: ");
+        int x = in.nextInt();
+        System.out.print("Введите количество столбцов: ");
+        int y = in.nextInt();
+
+        int[][] arr = new int[x][y];
+
+        // Ввод элементов массива
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        // Вывод элементов массива
+        System.out.println("Введенный массив:");
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                // arr[i][j] = in.nextInt();
+                System.out.print(arr[i] [j] + " ");
+            }
+            System.out.println();
+        }
+        in.close(); // Закрываем Scanner
     }
 }
