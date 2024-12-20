cfc2bf735988c19b65763742ffaf301f80af5528
mark <ivanchenko.mark@inbox.ru>
2024-12-20 15:30:38 +0700

    create new repo

---
+++ src/SummNummIntoString.java
@@ -0,0 +1,23 @@
+/*
+На ввод поступает два целых числа n, m - размер массива, сам массив и целое число k.
+
+Программа должна вывести сумму чисел в строке под номером k.
+
+Sample Input:
+
+        3 3
+        1 2 3
+        4 5 6
+        7 8 9
+        2
+Sample Output:
+
+        15
+*/
+
+
+public class SummNummIntoString {
+    public static void main(String[] args) {
+
+    }
+}
